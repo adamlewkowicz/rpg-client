8dfce550a5fecf0333f340e8bdfd2d8e18b85185
alk831 <adam54111@gmail.com>
2019-04-29 22:31:10 +0200

    scaffold game/location position selectors

---
+++ src/store/selectors/location.js
@@ -0,0 +1,49 @@
+import { createSelector } from 'reselect'
+
+export const characterCords = (state) => ({
+  x: state.character.data.positionX,
+  y: state.character.data.positionY
+});
+
+export const characerPosition = createSelector(
+  characterCords,
+  ({ x, y }) => ({
+    posX: x * 32,
+    posY: y * 48
+  })
+);
+
+export const locationMapEnds = (state) => ({
+  mapEndX: state.location.width - state.game.width,
+  mapEndY: state.location.height - state.game.height
+});
+
+
+export const locationMapPosition = createSelector(
+  characerPosition,
+  locationMapEnds,
+  ({ posX, posY }, { mapEndX, mapEndY }) => {
+
+    let isCameraLocked = false;
+    let mapX = posX;
+    let mapY = posY;
+
+    if (posX <= 0) {
+      mapX = 0;
+      isCameraLocked = true;
+    } else if (posX >= mapEndX) {
+      mapX = mapEndX;
+      isCameraLocked = true;
+    }
+
+    if (posY <= 0) {
+      mapY = 0;
+      isCameraLocked = true;
+    } else if (mapEndY >= posY) {
+      mapY = mapEndY;
+      isCameraLocked = true;
+    }
+
+    return { mapX, mapY, isCameraLocked };
+  }
+);
