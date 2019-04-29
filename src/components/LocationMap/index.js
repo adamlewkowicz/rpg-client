6c5e2f6a18b2229134f16b324f01809dfd1f4e96
alk831 <adam54111@gmail.com>
2019-04-29 22:11:38 +0200

    scaffold dynamic camera follow

--- src/components/LocationMap/index.js
+++ src/components/LocationMap/index.js
@@ -7,7 +7,7 @@ import { useMappedState } from 'redux-react-hook';
 import { useSpring, animated } from 'react-spring/konva';
 
 
-export const LocationMap = ({ character, game }) => {
+export const LocationMap = ({ character, game, location }) => {
   const [locationImg] = useImage(process.env.REACT_APP_LOCATION_IMG);
   // const {  } = useMappedState(state => state);
 
@@ -20,11 +20,46 @@ export const LocationMap = ({ character, game }) => {
     config: { mass: 1, tension: 150, friction: 50 }
   });
 
+  const cameraShouldFollow = () => {
+    /* X axis */
+    const { positionX: x, positionY: y } = character.data;
+
+    const charMapPosX = x * game.charWidth;
+    const charMapPosY = y * game.charHeight;
+
+    const mapEndLeftX = game.width / 2;
+    const mapEndRightX = location.width - game.width;
+
+    const mapEndTopY = game.height / 2;
+    const mapEndBottomY = location.height - game.height;
+
+    let mapX = charMapPosX;
+    let mapY = charMapPosY;
+
+  
+    if (mapX <= 0) {
+      mapX = 0;
+    } else if (charMapPosX >= mapEndRightX) {
+      mapX = mapEndRightX;
+    }
+
+    if (mapY  <= 0) {
+      mapY = 0;
+    } else if (mapY >= mapEndBottomY) {
+      mapY = mapEndBottomY;
+    }
+
+    return { x: mapX, y: mapY };
+  }
+  
+  console.log(cameraShouldFollow())
+  const { x, y } = cameraShouldFollow();
+
   return (
     <animated.Image
       image={locationImg}
-      x={0}
-      y={0}
+      x={x * -1}
+      y={y * -1}
     />
   )
 }
