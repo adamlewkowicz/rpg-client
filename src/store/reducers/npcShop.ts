c3858bf1e9425303b6f9dc386bb669b43aa57c2b
alk831 <adam54111@gmail.com>
2019-05-17 16:26:31 +0200

    scaffold npcShop reducer

---
+++ src/store/reducers/npcShop.ts
@@ -0,0 +1,40 @@
+import * as Actions from 'rpg-shared/action-types';
+import { NPC_SHOP_TRADE, $_NPC_SHOP_RESPONSE, NPC_SHOP_REQUEST } from 'rpg-shared/dist/consts';
+import { Item } from 'rpg-shared/objects';
+
+export interface NpcShop {
+  isLoading: boolean
+  opened: boolean
+  items: Item[]
+  buying: number[]
+  selling: number[]
+}
+
+const initialState = {
+  isLoading: false,
+  opened: false,
+  items: [],
+  buying: [],
+  selling: []
+}
+
+const npcShopReducer = (state = initialState, action: Actions.NpcShopActions) => {
+  switch(action.type) {
+    case NPC_SHOP_REQUEST: return {
+      ...state,
+      opened: true,
+      isLoading: true
+    }
+    case $_NPC_SHOP_RESPONSE: return {
+      ...state,
+      isLoading: false,
+      items: action.payload.items
+    }
+    case NPC_SHOP_TRADE: return {
+      ...state
+    }
+    default: return state;
+  }
+}
+
+export default npcShopReducer;
