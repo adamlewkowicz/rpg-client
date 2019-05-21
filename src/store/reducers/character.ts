e4f4964dc3c6f1d5612c973a2527e1278414b8ee
alk831 <adam54111@gmail.com>
2019-05-21 15:07:16 +0200

    refactor character reducer

---
+++ src/store/reducers/character.ts
@@ -0,0 +1,36 @@
+import {
+	$_LOAD_GAME, CHARACTER_UPDATE
+} from 'rpg-shared/consts';
+import { normalize } from '../../utils';
+import { CharacterState } from 'rpg-shared/store';
+import { CharacterActions } from 'rpg-shared/action-types/union-types'
+
+const initialState: CharacterState = {
+	data: null,
+	inventory: {},
+	inventorySize: 0
+}
+
+const characterReducer = (
+	state = initialState,
+	action: CharacterActions
+): CharacterState => {
+	switch(action.type) {
+		case $_LOAD_GAME: return {
+			...state,
+			data: action.payload.character,
+			inventory: normalize(action.payload.inventory),
+			inventorySize: action.payload.inventorySize
+		}
+		case CHARACTER_UPDATE: return {
+			...state,
+			data: {
+				...state.data,
+				...action.payload
+			}
+		}
+		default: return state;
+	}
+}
+
+export default characterReducer;
