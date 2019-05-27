efe19743db22bc1910cc00ce55f1a37218182573
alk831 <adam54111@gmail.com>
2019-05-27 11:35:33 +0200

    remove old components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux';
 import { store } from './store/index.ts';
 import { StoreContext } from 'redux-react-hook';
 
 import { Triggers } from './components/Triggers';
-import { Game } from './components/Game';
 import { GameRenderer } from './components/GameRenderer';
 import { Loading } from './components/Loading';
 import { UIContainer } from './components/UIContainer';
 
 class App extends React.Component {
-
-  componentDidMount() {
-  }
 
   render() {
     return (
@@ -22,8 +17,6 @@ class App extends React.Component {
         <StoreContext.Provider value={store}>
           <div className="App">
             <header className="App-header">
-              {/* <img src={logo} className="App-logo" alt="logo" /> */}
-              {/* <Game /> */}
               <UIContainer />
               <Loading
                 children={<GameRenderer />}
