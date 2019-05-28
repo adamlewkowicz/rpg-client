9d2bdf0ed6137adf9f44dc9b54b4818af000f99b
alk831 <adam54111@gmail.com>
2019-05-28 13:55:40 +0200

    add ts merged export

---
+++ src/store/actions/index.ts
@@ -0,0 +1,7 @@
+export * from './location';
+export * from './items';
+export * from './chat';
+export * from './battle';
+export * from './character';
+export * from './game';
+export * from './npc/index';
